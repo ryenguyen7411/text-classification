a08209360442cc0755aa9751a7e6c465bcdb571a
ryenguyen7411 <ryenguyen7411@gmail.com>
2016-11-21 21:15:50 +0700

    - Init project

---
+++ project/src/main/HelloWorld.java
@@ -0,0 +1,10 @@
+package main;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
